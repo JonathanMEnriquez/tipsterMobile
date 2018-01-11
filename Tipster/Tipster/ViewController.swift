408670477dee23b2f89e195769806d35dfd12866
user <user@users-MacBook-Pro.local>
2018-01-10 16:45:46 -0800

    initial commit

---
+++ Tipster/Tipster/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by user on 1/10/18.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
